0f1395d7dca5d38d0a4b6ed242d38121b349764f
GitPracticeRepositorys <96053405+GitPracticeRepositorys@users.noreply.github.com>
2023-04-09 18:16:51 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,37 +1,21 @@
 terraform {
+  /*
+  cloud {
+    workspaces {
+      name = "learn-terraform-eks"
+    }
+  }
+  */
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.47.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.4.3"
-    }
-
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0.4"
-    }
-
-    cloudinit = {
-      source  = "hashicorp/cloudinit"
-      version = "~> 2.2.0"
+      version = ">= 0"
     }
   }
-
-  required_version = "~> 1.3"
+  required_version = ">= 1.3"
+  ##...
 }
 
-provider "aws" {
-  region = "us-west-2"
-}
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
-}
-
-# Add more resources and modules as needed...
 
